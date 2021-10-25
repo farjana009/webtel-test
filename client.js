1a54b152523407d61758adfc8b85c851ec022682
farjana <jacosfarjana@gmail.com>
2021-10-26 01:14:55 +0600

    js path change

--- client.js
+++ client.js
@@ -697,7 +697,7 @@ app.use(cors());
 //app.use(express.static(path.join(__dirname, '/view')));
 
 app.use(express.static(__dirname + '/css/'));
-app.use(express.static(__dirname + '/js/'));
+app.use(express.static(__dirname + '/js'));
 app.use(express.static(__dirname + '/view/'));
 
 app.get('/', function(req, res) {
