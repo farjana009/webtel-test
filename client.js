54aa921c3602ddf06982dfa258e60700928efdc9
farjana <jacosfarjana@gmail.com>
2021-10-25 20:07:59 +0600

    js file modified

--- client.js
+++ client.js
@@ -704,5 +704,5 @@ function json2array2nd(json) {
     return result;
 }
 
-app.use(express.static(__dirname + '/index.html'));
+app.use(express.static(__dirname + 'index.html'));
 //app.use(express.static(path.join(__dirname,'front')));
